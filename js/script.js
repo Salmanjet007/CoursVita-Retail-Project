bd5a1603a691597b32b35715b5ac9ddb8d2317e8
Salmanjet007 <salmanjet007@gmail.com>
2025-01-31 16:28:06 +0530

    Home Page edited

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for hover effects
-document.querySelectorAll('.zoom-effect').forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.style.transform = 'scale(1.2)';
-    });
-    item.addEventListener('mouseout', () => {
-        item.style.transform = 'scale(1)';
-    });
-});
